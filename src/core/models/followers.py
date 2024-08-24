829d79bde734e266ce4affbf34df954523b98d95
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-08-24 09:20:26 +0200

    add UniqueConstraint

--- src/core/models/followers.py
+++ src/core/models/followers.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.core import Base
@@ -6,6 +6,9 @@ from src.core import Base
 
 class Followers(Base):
     __tablename__ = "user_following"
+    __table_args__ = (
+        UniqueConstraint("user_id", "follower_id", name="unique_user_follower_pair"),
+    )
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
     follower_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
