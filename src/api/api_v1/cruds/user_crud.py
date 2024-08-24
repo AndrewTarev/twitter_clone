04d87f93420eb5826c1c5cae495e9cd5fb645cfb
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-08-24 09:22:53 +0200

    add follow router

--- src/api/api_v1/cruds/user_crud.py
+++ src/api/api_v1/cruds/user_crud.py
@@ -1,7 +1,10 @@
-from sqlalchemy import select
+from fastapi import HTTPException
+from sqlalchemy import and_, select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
+from src.core import Followers
 from src.core.models.security_key import SecurityKey
 from src.core.models.users import User
 from src.utils.logging_config import logger
@@ -22,3 +25,48 @@ async def get_user(
     logger.info(f"headers api-key: {api_key}")
     logger.info(f"user: {result}")
     return result
+
+
+async def get_user_by_id_crud(
+    session: AsyncSession,
+    user_id: int,
+) -> User | None:
+    stmt = (
+        select(User)
+        .options(selectinload(User.followers))
+        .options(selectinload(User.following))
+        .where(User.id == user_id)
+    )
+    user = await session.execute(stmt)
+    user = user.scalars().first()
+    return user
+
+
+async def user_follow(
+    session: AsyncSession,
+    user_id: int,
+    user: User,
+):
+    try:
+        follow: Followers = Followers(user_id=user.id, follower_id=user_id)
+        session.add(follow)
+        await session.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="Follow already exists")
+
+
+async def user_unfollow(
+    session: AsyncSession,
+    user_id: int,
+    user: User,
+) -> None:
+    stmt = select(Followers).where(
+        and_(Followers.user_id == user.id, Followers.follower_id == user_id)
+    )
+    result = await session.execute(stmt)
+    follow = result.scalars().first()
+    logger.info(f"follow: {follow}")
+
+    if follow:
+        await session.delete(follow)
+        await session.commit()
--- src/api/api_v1/routers/users_route.py
+++ src/api/api_v1/routers/users_route.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
 
+from fastapi import APIRouter, Depends, HTTPException, Path
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.api.api_v1.cruds.user_crud import (
+    get_user_by_id_crud,
+    user_follow,
+    user_unfollow,
+)
 from src.api.dependencies.user import get_user_dependency
 from src.core import User
 from src.core.config import settings
+from src.core.db_helper import db_helper
 from src.core.schemas.users_schema import UserInfoResponse
 
 router = APIRouter(prefix=settings.api.users, tags=["Users"])
@@ -11,3 +20,34 @@ router = APIRouter(prefix=settings.api.users, tags=["Users"])
 @router.get("/me", response_model=UserInfoResponse)
 async def get_current_user(user: User = Depends(get_user_dependency)):
     return {"result": True, "user": user}
+
+
+@router.get("/{id}", response_model=UserInfoResponse)
+async def get_user_by_id(
+    id: int,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    current_user = await get_user_by_id_crud(session=session, user_id=id)
+    if current_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"result": True, "user": current_user}
+
+
+@router.post("/{id}/follow", status_code=201)
+async def follow(
+    id: Annotated[int, Path],
+    user: User = Depends(get_user_dependency),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    await user_follow(session=session, user_id=id, user=user)
+    return {"result": True}
+
+
+@router.delete("/{id}/follow", status_code=204)
+async def unfollow(
+    id: Annotated[int, Path],
+    user: User = Depends(get_user_dependency),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    await user_unfollow(session=session, user_id=id, user=user)
+    return {"result": True}
