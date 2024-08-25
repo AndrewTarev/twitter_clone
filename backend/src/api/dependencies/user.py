5579e7756e1f2d8caf077e7fe93d7cf675c4e6a1
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-08-25 11:41:08 +0200

    fix function

--- backend/src/api/dependencies/user.py
+++ backend/src/api/dependencies/user.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from backend.src.api.api_v1.cruds.user_crud import get_user
+from backend.src.core import User
 from backend.src.core.db_helper import db_helper
 from fastapi import Depends, Header
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,6 +10,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 async def get_user_dependency(
     api_key: Annotated[str | None, Header()],
     session: AsyncSession = Depends(db_helper.session_getter),
-):
+) -> User:
     user = await get_user(session=session, api_key=api_key)
     return user
