695a398db09ec2b4e2d78fee785c9a978b024ca3
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-08-26 02:35:50 +0200

    refactoring file handler

--- backend/src/api/api_v1/cruds/media_crud.py
+++ backend/src/api/api_v1/cruds/media_crud.py
@@ -10,27 +10,37 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def handle_uploaded_file(file: UploadFile, session: AsyncSession, user: User):
-    relative_directory = pathlib.Path(
-        __file__
-    ).parent.parent.parent.parent.parent.parent
-    abs_path = f"{relative_directory}/frontend/static/images"
+    # Определяем директорию для сохранения
+    relative_directory = (
+        pathlib.Path(__file__).resolve().parent.parent.parent.parent.parent.parent
+        / "frontend/static/images"
+    )
+    os.makedirs(
+        relative_directory, exist_ok=True
+    )  # Создаем директорию, если не существует
+
     logger.info(f"relative path: {relative_directory}")
 
-    # создаем уникальное имя файла
+    # Создаем уникальное имя файла
     _, file_extension = file.content_type.split("/")
     timestamp: int = int(time.time() * 1000)
     new_file_name = f"{timestamp}_{user.id}.{file_extension}"
-    file_location: str = os.path.join(abs_path, new_file_name)
+    file_location: str = os.path.join(relative_directory, new_file_name)
+
     logger.info(f"Uploading {file_location}")
 
     media: Media = Media(link=file_location, file_name=new_file_name)
 
-    async with aiofiles.open(file_location, "wb") as out_file:
-        content = await file.read()
-        await out_file.write(content)
+    try:
+        async with aiofiles.open(file_location, "wb") as out_file:
+            content = await file.read()
+            await out_file.write(content)
 
-    session.add(media)
-    await session.commit()
-    logger.info(f"Создан медиа файл - {media}")
+        session.add(media)
+        await session.commit()
+        logger.info(f"Создан медиа файл - {media}")
+    except Exception as e:
+        logger.error(f"Ошибка при загрузке файла: {e}")
+        raise
 
     return media.id
