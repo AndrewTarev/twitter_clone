891376d936bf7f509d2341cedd4c0c15e9e9841b
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-09-01 08:18:47 +0200

    add security

--- backend/src/api/dependencies/user.py
+++ backend/src/api/dependencies/user.py
@@ -1,9 +1,8 @@
-from fastapi.security import APIKeyHeader
-
 from backend.src.api.api_v1.cruds.user_crud import get_user
 from backend.src.core import User
 from backend.src.core.db_helper import db_helper
 from fastapi import Depends, Security
+from fastapi.security import APIKeyHeader
 from sqlalchemy.ext.asyncio import AsyncSession
 
 API_KEY_HEADER = APIKeyHeader(name="api-key")
