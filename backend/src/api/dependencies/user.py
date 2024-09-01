5c7dfc2e5c406a503323c4f7debc004db6a7e7ad
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-09-01 08:17:01 +0200

    add security

--- backend/src/api/dependencies/user.py
+++ backend/src/api/dependencies/user.py
@@ -1,14 +1,16 @@
-from typing import Annotated
+from fastapi.security import APIKeyHeader
 
 from backend.src.api.api_v1.cruds.user_crud import get_user
 from backend.src.core import User
 from backend.src.core.db_helper import db_helper
-from fastapi import Depends, Header
+from fastapi import Depends, Security
 from sqlalchemy.ext.asyncio import AsyncSession
+
+API_KEY_HEADER = APIKeyHeader(name="api-key")
 
 
 async def get_user_dependency(
-    api_key: Annotated[str, Header()],
+    api_key: str = Security(API_KEY_HEADER),
     session: AsyncSession = Depends(db_helper.session_getter),
 ) -> User | None:
     user = await get_user(session=session, api_key=api_key)
